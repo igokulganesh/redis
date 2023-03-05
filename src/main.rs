fa8f0d48f563528bb1f7bd08e1a8c198e77dbc1c
Gokul Ganesh I <igokulganesh@gmail.com>
2023-03-05 13:09:14 +0530

    Respond to PING

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::io::{Read, Write};
+use std::net::TcpListener;
+
 fn main() {
-    println!("Hello From Redis");
+    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(mut stream) => {
+                println!("accepted new connection");
+
+                let mut buf = [0; 512];
+                stream.read(&mut buf).unwrap();
+
+                stream.write("+PONG\r\n".as_bytes()).unwrap();
+            }
+            Err(e) => {
+                println!("error: {}", e);
+            }
+        }
+    }
 }
